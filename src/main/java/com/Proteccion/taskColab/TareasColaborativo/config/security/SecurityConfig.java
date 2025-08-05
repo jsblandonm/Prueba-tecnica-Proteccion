23dc85c62c79ede2d26dcd98bc9ebb142bbc95f8
jsblandonm <sebasblandonmontoya@gmail.com>
2025-08-05 12:45:48 -0500

    permission h2

--- src/main/java/com/Proteccion/taskColab/TareasColaborativo/config/security/SecurityConfig.java
+++ src/main/java/com/Proteccion/taskColab/TareasColaborativo/config/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                         authRequest
                                 .requestMatchers("/auth/**","/swagger-ui/**",
                                         "/v3/api-docs/**",
-                                        "/swagger-ui.html").permitAll()
+                                        "/swagger-ui.html", "/h2-console/**" ).permitAll()
                                 .anyRequest().authenticated()
                 )
 
